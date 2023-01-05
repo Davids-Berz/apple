4693a86989666d3b8bf9c3415e91e0df5ab10f8d
David Saldivar <davids.berz@gmail.com>
2023-01-04 18:09:42 -0600

    agregando objeto al model

--- src/main/java/com/apple/apple/controller/IndexController.java
+++ src/main/java/com/apple/apple/controller/IndexController.java
@@ -2,8 +2,11 @@ package com.apple.apple.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.apple.apple.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -14,5 +17,16 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("titulo", "Creando un Controlador");
         return "index";
+    }
+
+    @GetMapping("/perfil")
+    public String perfil(ModelMap map) {
+        final Usuario usuario = new Usuario();
+        usuario.setNombre("David");
+        usuario.setApellido("Saldivar");
+        map
+                .addAttribute("usuario", usuario)
+                .addAttribute("titulo", "Usuario: " + usuario.getNombre());
+        return "perfil";
     }
 }
