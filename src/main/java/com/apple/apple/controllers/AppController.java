4488a8639f6353c467d6337651fd4dc38a6a29ff
David Saldivar <davids.berz@gmail.com>
2023-01-09 16:52:07 -0600

    agregando errorhandler para aritmetica

---
+++ src/main/java/com/apple/apple/controllers/AppController.java
@@ -0,0 +1,15 @@
+package com.apple.apple.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    
+    @GetMapping("/")
+    public String index(Model model) {
+        Integer i = 100/0;
+        return "index";
+    }
+}
