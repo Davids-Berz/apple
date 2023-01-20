29bc593e5e4017d9ddf9018721f32870892ff19d
David Saldivar <davids.berz@gmail.com>
2023-01-20 15:51:59 -0600

    agregar editar

--- src/main/java/com/apple/apple/controllers/ClienteController.java
+++ src/main/java/com/apple/apple/controllers/ClienteController.java
@@ -6,27 +6,27 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import com.apple.apple.models.dao.IClienteDao;
 import com.apple.apple.models.entity.Cliente;
+import org.springframework.web.bind.support.SessionStatus;
 
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
-    
+
 
     @Autowired
     @Qualifier("clienteDaoJPA")
     private IClienteDao clienteDao;
 
-    @RequestMapping(value="/listar", method = RequestMethod.GET)
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
     private String listsar(Model model) {
         model.addAttribute("titulo", "Listado de cliente");
         model.addAttribute("clientes", clienteDao.findAll());
-        return  "listar";
+        return "listar";
     }
 
     @GetMapping("/form")
@@ -37,8 +37,8 @@ public class ClienteController {
         return "form";
     }
 
-    @RequestMapping(value="/form", method=RequestMethod.POST)
-    public String guardar(Model model, @Valid Cliente cliente, BindingResult result) {
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String guardar(Model model, @Valid Cliente cliente, BindingResult result, SessionStatus status) {
         //BindingResult siempre despues de @Valid y despues de mas parametros
         if (result.hasErrors()) {
             model.addAttribute("titulo", "Formulario Cliente");
@@ -46,7 +46,21 @@ public class ClienteController {
             return "form";
         }
         clienteDao.save(cliente);
+        status.setComplete();
         return "redirect:listar";
     }
-    
+
+    @RequestMapping(value = "/form/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        model.addAttribute("titulo", "Editar Cliente");
+        Cliente cliente = new Cliente();
+        if (id>0) {
+            cliente = clienteDao.findOne(id);
+        } else {
+            return "redirect:form";
+        }
+        model.addAttribute("cliente", cliente);
+        return "form";
+    }
+
 }
