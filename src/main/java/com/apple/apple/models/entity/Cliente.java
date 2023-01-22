316d02e70c1dfa058072c124af867ebde22b0e03
David Saldivar <davids.berz@gmail.com>
2023-01-22 03:45:33 -0600

    date form in html

--- src/main/java/com/apple/apple/models/entity/Cliente.java
+++ src/main/java/com/apple/apple/models/entity/Cliente.java
@@ -16,6 +16,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="clientes")
@@ -39,14 +40,10 @@ public class Cliente implements Serializable {
 
     @Column(name="create_at")
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createAt; 
     
      private static final Long serialVersionUID = 1L;
-
-     @PrePersist
-     public void prePersist() {
-        createAt = new Date();
-     }
 
     public Long getId() {
         return id;
