c01a06ed77eb7cc6a07aafefdc3d2273d699404b
David Saldivar <davids.berz@gmail.com>
2023-02-01 01:57:38 -0600

    add table factura in ver html

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,8 @@
-insert into clientes (id, nombre, apellido, email, create_at, foto) values (1, 'david', 'saldivar', 'david@gmail.com', '2023-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('david', 'saldivar', 'david@gmail.com', '2023-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('Dianey', 'Tapia', 'dt@gmail.com', '2022-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('Juan', 'Hernan', 'jh@gmail.com', '2022-11-10','');
+insert into productos(nombre, precio, create_at) values ('Sony Camara Digital DSC-W320', 23990,'2023-01-19');
+insert into productos(nombre, precio, create_at) values ('Apple iphone 14 pro max', 34499, '2023-04-04');
+insert into productos(nombre, precio, create_at) values ('Whirlpool WM1211D Horno de Microondas', 3499, '2021-11-12');
+insert into productos(nombre, precio, create_at) values (' Switch Neon 32GB Version 1.1', 6347, '2021-11-12');
+insert into productos(nombre, precio, create_at) values ('Nintendo Switch Lite Hand-Held', 5348, '2022-06-01');
