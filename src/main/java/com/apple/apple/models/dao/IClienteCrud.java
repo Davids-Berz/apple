150a933c7752909d650a08ac4ab0824a039c9a84
David Saldivar <davids.berz@gmail.com>
2023-02-09 02:34:13 -0600

    left join del cliente

--- src/main/java/com/apple/apple/models/dao/IClienteCrud.java
+++ src/main/java/com/apple/apple/models/dao/IClienteCrud.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface IClienteCrud extends JpaRepository<Cliente,Long> {
 
-    @Query("select c from Cliente c join fetch c.facturas f where c.id = ?1")
+    @Query("select c from Cliente c left join c.facturas f where c.id = ?1")
     public Cliente fetchByIdWithFacturas(Long id);
 }
