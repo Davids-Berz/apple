b86bef3a4ff3f251b7d38581da98b64e63951f68
David Saldivar <davids.berz@gmail.com>
2023-02-09 19:41:31 -0600

    SpringSecurityConfig roles y usuarios

---
+++ src/main/java/com/apple/apple/SpringSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.apple.apple;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar")
+                .permitAll()
+                .requestMatchers("/ver/**").hasAnyRole("USER")
+                .requestMatchers("/uploads/**").hasAnyRole("USER")
+                .requestMatchers("/form/**").hasAnyRole("ADMIN")
+                .requestMatchers("/delete/**").hasAnyRole("ADMIN")
+                .requestMatchers("/factura/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .httpBasic()
+                .and().build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User
+                        .withUsername("admin")
+                        .password(passwordEncoder().encode("admin"))
+                        .roles("USER","ADMIN")
+                        .build());
+        manager.createUser(
+                User
+                        .withUsername("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles("USER")
+                        .build());
+        return manager;
+    }
+
+    @Bean
+    AuthenticationManager authManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder())
+                .and().build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
