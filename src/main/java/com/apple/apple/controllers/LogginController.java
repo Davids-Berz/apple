cb850db29cac11a92be3c78410ba9d314e1b6856
David Saldivar <davids.berz@gmail.com>
2023-02-13 03:18:53 -0600

    Logout

--- src/main/java/com/apple/apple/controllers/LogginController.java
+++ src/main/java/com/apple/apple/controllers/LogginController.java
@@ -12,14 +12,20 @@ import java.security.Principal;
 public class LogginController {
 
     @GetMapping("/login")
-    public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes flash) {
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model, Principal principal, RedirectAttributes flash) {
         if (principal != null) {
             flash.addFlashAttribute("info", "ya has iniciado sesion anteriormente");
             return "redirect:/";
         }
 
         if (error != null) {
-            model.addAttribute("error","Error al iniciar session, usuario o contraseña incorrecta");
+            model.addAttribute("error", "Error al iniciar session, usuario o contraseña incorrecta");
+
+        }
+        if (logout != null) {
+            model.addAttribute("info", "Session Cerrada!");
 
         }
         return "login";
