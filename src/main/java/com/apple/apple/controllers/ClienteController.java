27db6b2c5cbe0a77306e0c9e90a4b5d2159459c4
David Saldivar <davids.berz@gmail.com>
2023-02-13 06:44:14 -0600

    Role con metodo en Controlador

--- src/main/java/com/apple/apple/controllers/ClienteController.java
+++ src/main/java/com/apple/apple/controllers/ClienteController.java
@@ -16,6 +16,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,6 +36,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.UUID;
 
 
@@ -83,6 +87,12 @@ public class ClienteController {
 
         if (auth != null) {
             LOG.info("Utilizando SecurityContextHolder.getContext().getAuthentication()");
+        } else {
+            LOG.info("No tienes Acceso");
+        }
+
+        if (hasRole("ROLE_ADMIN")) {
+            LOG.info("Hola ".concat(auth.getName()).concat(" tienes acceso"));
         }
 
         Pageable pageRequest = PageRequest.of(page, 4);
@@ -187,6 +197,24 @@ public class ClienteController {
             }
         }
         return "redirect:/listar";
+    }
+
+    private boolean hasRole(String role) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (context == null) return false;
+        Authentication auth = context.getAuthentication();
+        
+        if (auth == null) return false;
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        return authorities.contains(new SimpleGrantedAuthority(role));
+        /*for (GrantedAuthority authority:
+             authorities) {
+            if (role.equals(authority.getAuthority())) {
+                LOG.info("Hola ".concat(auth.getName()).concat("tu rol es: ").concat(authority.getAuthority()));
+                return true;
+            }
+        }
+        return false;*/
     }
 
 }
