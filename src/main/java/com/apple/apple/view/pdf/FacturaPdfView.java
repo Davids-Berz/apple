21357e70d49f1359b23be38e2bd1b9d12bc7d5e0
David Saldivar <davids.berz@gmail.com>
2023-02-15 04:57:03 -0600

    agregando las tablas al pdf

--- src/main/java/com/apple/apple/view/pdf/FacturaPdfView.java
+++ src/main/java/com/apple/apple/view/pdf/FacturaPdfView.java
@@ -1,7 +1,10 @@
 package com.apple.apple.view.pdf;
 
 import com.apple.apple.models.entity.Factura;
+import com.apple.apple.models.entity.ItemFactura;
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,17 +24,40 @@ public class FacturaPdfView extends AbstractPdfView {
         Factura factura = (Factura) model.get("factura");
 
         PdfPTable table = new PdfPTable(1);
+        table.setSpacingAfter(20);
         table.addCell("Datos del Cliente");
         table.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
         table.addCell(factura.getCliente().getEmail());
 
         PdfPTable table2 = new PdfPTable(1);
-        table.addCell("Datos de la Factura");
-        table.addCell("Folio: " + factura.getId());
-        table.addCell("Descripcion: " + factura.getDescripcion());
-        table.addCell("Fecha: " + factura.getCreateAt());
+        table2.setSpacingAfter(20);
+        table2.addCell("Datos de la Factura");
+        table2.addCell("Folio: " + factura.getId());
+        table2.addCell("Descripcion: " + factura.getDescripcion());
+        table2.addCell("Fecha: " + factura.getCreateAt());
 
         document.add(table);
         document.add(table2);
+
+        PdfPTable table3 = new PdfPTable(4);
+        table3.addCell("Producto");
+        table3.addCell("Precio");
+        table3.addCell("Cantidad");
+        table3.addCell("Total");
+
+        for (ItemFactura item: factura.getItems()) {
+            table3.addCell(item.getProducto().getNombre());
+            table3.addCell(item.getProducto().getPrecio().toString());
+            table3.addCell(item.getCantidad().toString());
+            table3.addCell(item.calcularImporte().toString());
+        }
+
+        PdfPCell cell = new PdfPCell(new Phrase("total: "));
+        cell.setColspan(3);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+        table3.addCell(cell);
+        table3.addCell(factura.getTotal().toString());
+
+        document.add(table3);
     }
 }
