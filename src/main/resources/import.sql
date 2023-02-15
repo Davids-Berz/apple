5683ef79e1c53b97bf22985807c677c7f9970baa
David Saldivar <davids.berz@gmail.com>
2023-02-15 01:47:20 -0600

    LocaleController y seguridad

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,12 @@
 insert into clientes (nombre, apellido, email, create_at, foto) values ('david', 'saldivar', 'david@gmail.com', '2023-01-18','');
 insert into clientes (nombre, apellido, email, create_at, foto) values ('Dianey', 'Tapia', 'dt@gmail.com', '2022-01-18','');
 insert into clientes (nombre, apellido, email, create_at, foto) values ('Juan', 'Hernan', 'jh@gmail.com', '2022-11-10','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('david', 'saldivar', 'david@gmail.com', '2023-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('Dianey', 'Tapia', 'dt@gmail.com', '2022-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('Juan', 'Hernan', 'jh@gmail.com', '2022-11-10','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('david', 'saldivar', 'david@gmail.com', '2023-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('Dianey', 'Tapia', 'dt@gmail.com', '2022-01-18','');
+insert into clientes (nombre, apellido, email, create_at, foto) values ('Juan', 'Hernan', 'jh@gmail.com', '2022-11-10','');
 
 insert into productos(nombre, precio, create_at) values ('Sony Camara Digital DSC-W320', 23990,'2023-01-19');
 insert into productos(nombre, precio, create_at) values ('Apple iphone 14 pro max', 34499, '2023-04-04');
