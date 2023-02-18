a46c98df47ef3bea066135e57084114140631be1
David Saldivar <davids.berz@gmail.com>
2023-02-18 06:10:13 -0600

    agregando controller

---
+++ src/main/java/com/apple/apple/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package com.apple.apple.controller;
+
+import com.apple.apple.models.entity.Cliente;
+import com.apple.apple.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteController {
+
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping("/clientes")
+    public List<Cliente> list() {
+        return clienteService.findAll();
+    }
+}
