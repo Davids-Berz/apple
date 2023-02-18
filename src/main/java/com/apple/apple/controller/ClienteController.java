2bc242ff0e645487e4641f2310821afb2820c482
David Saldivar <davids.berz@gmail.com>
2023-02-18 06:39:32 -0600

    Cors compartir recursos con Api-Rest

--- src/main/java/com/apple/apple/controller/ClienteController.java
+++ src/main/java/com/apple/apple/controller/ClienteController.java
@@ -3,12 +3,14 @@ package com.apple.apple.controller;
 import com.apple.apple.models.entity.Cliente;
 import com.apple.apple.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteController {
--- src/main/java/com/apple/apple/service/ClienteServiceImpl.java
+++ src/main/java/com/apple/apple/service/ClienteServiceImpl.java
@@ -3,10 +3,12 @@ package com.apple.apple.service;
 import com.apple.apple.models.dao.IClienteDao;
 import com.apple.apple.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class ClienteServiceImpl implements IClienteService{
 
     @Autowired
